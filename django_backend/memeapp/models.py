57919fff135bb1a907c791d50e6c71ee3cdd3645
vadim <vadim>
2021-06-25 20:18:15 +0000

    bugs

--- django_backend/memeapp/models.py
+++ django_backend/memeapp/models.py
@@ -8,6 +8,20 @@ from uuid import uuid4
 
 import schedule
 
+
+def path_and_rename_template(instance, filename):
+    # path to MEDIA will be added automatically
+    upload_to = settings.TEMPLATES_FOLDER
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
 class Template(models.Model):
     image = models.ImageField(upload_to=path_and_rename_template, default=None)
     published_on = models.DateTimeField(auto_now_add=True)
